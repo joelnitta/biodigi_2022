afadeaa6de65c9b12c0bb70b8399a44d7c566101
Joel Nitta <joelnitta@gmail.com>
2022-07-12 09:54:05 +0900

    Add script to make hex sticker

---
+++ R/make_hex.R
@@ -0,0 +1,7 @@
+library(hexSticker)
+
+imgurl <- "images/taxi.png"
+sticker(
+  imgurl, package = "taxastand",
+  p_size = 20, s_x = 1, s_y = .75, s_width = .5, s_height = .5,
+  filename = "images/taxastand_sticker.png")
